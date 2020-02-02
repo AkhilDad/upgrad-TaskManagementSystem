398267e002de785abddb49754093a11537254813
deepakagra <deepakagra@gmail.com>
2020-02-02 14:29:20 +0530

    using customized exception

---
+++ src/com/upgrad/tms/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.upgrad.tms.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message){
+        super(message);
+    }
+
+}
--- src/com/upgrad/tms/menu/MenuFactory.java
+++ src/com/upgrad/tms/menu/MenuFactory.java
@@ -1,5 +1,7 @@
 package com.upgrad.tms.menu;
 
+import com.upgrad.tms.exception.NotFoundException;
+
 class MenuFactory {
     public static OptionsMenu getMenuByType(OptionsMenuType optionsMenuType) {
         switch (optionsMenuType) {
@@ -8,8 +10,8 @@ class MenuFactory {
             case ASSIGNEE:
                 return new AssigneeMenu();
         }
-        throw new RuntimeException("Options Menu Type not supported");
-        
+        throw new NotFoundException("Options Menu Type not supported");
+
     }
 
 }
