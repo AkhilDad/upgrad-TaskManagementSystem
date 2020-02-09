525680efbf89ba19beabd5d6afcb69bcbf47b197
Akhil Dad <dadakhil@gmail.com>
2020-02-09 19:58:18 +0530

    Cyclic barrier

--- src/com/upgrad/tms/menu/AssigneeMenu.java
+++ src/com/upgrad/tms/menu/AssigneeMenu.java
@@ -21,8 +21,7 @@ import com.upgrad.tms.util.TaskStatus;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 class AssigneeMenu implements OptionsMenu {
@@ -105,10 +104,16 @@ class AssigneeMenu implements OptionsMenu {
         List<Task> multipleTask = getMultipleTask();
         if (multipleTask.size() > 2) {
             CountDownLatch countDownLatch = new CountDownLatch(multipleTask.size()- 1);
+            CyclicBarrier cyclicBarrier = new CyclicBarrier(multipleTask.size() - 1 );
             Thread thread = new Thread(new ParentWorker(assigneeRepository, countDownLatch, multipleTask.get(0)));
             thread.start();
             for (int i = 1; i < multipleTask.size(); i++) {
-                new Thread(new ChildWorker(assigneeRepository, multipleTask.get(i), countDownLatch)).start();
+                new Thread(new ChildWorker(assigneeRepository, multipleTask.get(i), countDownLatch, cyclicBarrier)).start();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
