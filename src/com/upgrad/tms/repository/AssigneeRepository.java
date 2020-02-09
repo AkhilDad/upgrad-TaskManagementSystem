cab22a15c53625a5440327e43e0ebd31a595eaf8
Akhil Dad <dadakhil@gmail.com>
2020-02-09 10:51:14 +0530

    Solution to race condition

--- src/com/upgrad/tms/repository/AssigneeRepository.java
+++ src/com/upgrad/tms/repository/AssigneeRepository.java
@@ -60,11 +60,13 @@ public class AssigneeRepository {
 
     }
 
-    public void updateListToFile() throws IOException {
+    public synchronized void updateListToFile() throws IOException {
         try (
                 FileOutputStream fileOutputStream = new FileOutputStream(new File(Constants.ASSIGNEE_FILE_NAME));
                 ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream)) {
+            System.out.println("Writing in the file started by thread: "+Thread.currentThread().getName());
             outputStream.writeObject(assigneeList);
+            System.out.println("Writing in the file completed by thread: "+Thread.currentThread().getName());
         }
 
     }
